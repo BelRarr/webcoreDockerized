7d72767957c3afa6d6acb9518f595c6a2c40594c
Tidjani Belmansour <tbelmansour@cofomo.com>
2020-11-19 10:56:25 -0500

    Ajoutez des fichiers projet.

---
+++ webcoreDockerized/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webcoreDockerized.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
